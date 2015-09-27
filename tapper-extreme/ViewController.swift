64a95e3cff6026aa788a4213900f5d462239daef
bolans01 <bolans01@gmail.com>
2015-09-26 20:36:02 -0400

    added functionality to restart game if null text is input

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -9,21 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Properties
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    
+    //Outlets
     @IBOutlet weak var logoImg: UIImageView!
     @IBOutlet weak var howManyTapsTxt: UITextField!
     @IBOutlet weak var playBtn: UIButton!
     
     @IBOutlet weak var tapBtn: UIButton!
     @IBOutlet weak var tapsLbl: UILabel!
+    
+    @IBAction func onCoinTapped(sender: UIButton!) {
+        currentTaps++
+        updateTapsLbl()
+        
+        if isGameOver() {
+            restartGame()
+        }
+    }
 
     @IBAction func onPlayBtnPressed(sender: UIButton!){
-        logoImg.hidden = true
-        playBtn.hidden = true
-        howManyTapsTxt.hidden = true
+
+        if howManyTapsTxt.text != nil && howManyTapsTxt != "" {
+            
+            logoImg.hidden = true
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            updateTapsLbl()
+        }
+    }
+    
+    
+    func restartGame() {
+        maxTaps = 0
+        howManyTapsTxt.text = ""
         
-        tapBtn.hidden = false
-        tapsLbl.hidden = false
+        logoImg.hidden = false
+        playBtn.hidden = false
+        howManyTapsTxt.hidden = false
+        
+        tapBtn.hidden = true
+        tapsLbl.hidden = true
+    }
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps{
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func updateTapsLbl(){
+        tapsLbl.text = "\(currentTaps) Taps"
     }
 }
 
